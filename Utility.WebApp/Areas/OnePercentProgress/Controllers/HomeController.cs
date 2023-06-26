e80e4535a29a6badbf654138b568a94f55bc7266
Chintan Bilimoria <chintan.bilimoria@gmail.com>
2023-06-26 18:23:44 +0300

    Add project files.

---
+++ Utility.WebApp/Areas/OnePercentProgress/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Utility.WebApp.Areas.OnePercentProgress.Controllers
+{
+    [Area("OnePercentProgress")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewProject(string id)
+        {
+            return View();
+        }
+
+        public IActionResult ManageProject(string id)
+        {
+            return View();
+        }
+    }
+}
